f8a67f5bbc4d3569a2915fca298dd23d28b223f5
kd puvvadi <kdpuvvadi@gmail.com>
2023-08-11 21:45:29 +0530

    bump tf(cf) version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.8.0"
+      version = "4.12.0"
     }
     vercel = {
       source  = "vercel/vercel"
