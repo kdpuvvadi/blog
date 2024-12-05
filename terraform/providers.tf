a29e463855c994a53571184f6e4d8f99c24eb511
KD Puvvadi <rao@puvvadi.me>
2024-12-05 17:16:16 +0530

    :facepalm:

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
 # Providers
 
 terraform {
-  required_version = ">= 4.47.0"
+  required_version = ">= 1.3.0"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.17.0"
+      version = "4.47.0"
     }
   }
   cloud {
