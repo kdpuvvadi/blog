e48d7c51c38dad1753c35524aa53005176b3601b
KD Puvvadi <7326255+kdpuvvadi@users.noreply.github.com>
2026-01-06 18:00:12 +0530

    refactor: update Cloudflare configuration and remove outputs file

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,69 +1,66 @@
 # Cloudflare project deployment and DNS record for the project
-
 resource "cloudflare_pages_project" "blog_pages_project" {
   account_id        = "785a966c2bc174e625f1140eedfd6a89"
   name              = "blog"
   production_branch = "main"
 
-  source {
+  source = {
     type = "github"
-    config {
-      owner                         = "kdpuvvadi"
-      repo_name                     = "blog"
-      production_branch             = "main"
-      pr_comments_enabled           = true
-      deployments_enabled           = true
-      production_deployment_enabled = true
-      preview_deployment_setting    = "all"
-      preview_branch_includes       = ["*"]
-      preview_branch_excludes       = ["main", "prod"]
+    config = {
+      owner     = "kdpuvvadi"
+      repo_name = "blog"
+
+      production_branch       = "main"
+      pr_comments_enabled     = true
+      deployments_enabled     = true
+      preview_branch_includes = ["*"]
+      preview_branch_excludes = ["main", "prod"]
     }
   }
 
-  build_config {
-    build_command       = "bundle install && bundle exec jekyll build --config $CONFIG_FILE"
-    destination_dir     = "_site"
-    root_dir            = ""
-    web_analytics_tag   = "359f78cfd77a4d92be4986bff4c02cc4"
-    web_analytics_token = "3cb459fe825f4dfc987979a2e512dd34"
-    build_caching       = true
+  build_config = {
+    build_command   = "bundle install && bundle exec jekyll build --config $CONFIG_FILE"
+    destination_dir = "_site"
+    root_dir        = ""
+    build_caching   = true
   }
 
-  deployment_configs {
-    preview {
-      environment_variables = {
-        CONFIG_FILE    = "_config_preview.yml"
-        JEKYLL_ENV     = "development"
-        BUNDLE_WITHOUT = ""
-        RUBY_VERSION   = "3.4.4"
+  deployment_configs = {
+    preview = {
+      env_vars = {
+        CONFIG_FILE  = { value = "_config_preview.yml", type = "plain_text" }
+        JEKYLL_ENV   = { value = "development", type = "plain_text" }
+        RUBY_VERSION = { value = "3.4.4", type = "plain_text" }
       }
       fail_open = true
+      build_image_major_version = 3
     }
-    production {
-      environment_variables = {
-        CONFIG_FILE    = "_config.yml"
-        JEKYLL_ENV     = "production"
-        BUNDLE_WITHOUT = ""
-        RUBY_VERSION   = "3.4.4"
+
+    production = {
+      env_vars = {
+        CONFIG_FILE  = { value = "_config.yml", type = "plain_text" }
+        JEKYLL_ENV   = { value = "production", type = "plain_text" }
+        RUBY_VERSION = { value = "3.4.4", type = "plain_text" }
       }
       fail_open = true
+      build_image_major_version = 3
     }
   }
 }
+
 
 resource "cloudflare_pages_domain" "cloudflare_blog_domain" {
   account_id   = "785a966c2bc174e625f1140eedfd6a89"
   project_name = cloudflare_pages_project.blog_pages_project.name
-  domain       = "puvvadi.net"
-  depends_on   = [cloudflare_record.cloudflare_blog_cname_record]
+  name         = "puvvadi.net"
+  depends_on   = [cloudflare_dns_record.cloudflare_blog_cname_record]
 }
 
-resource "cloudflare_record" "cloudflare_blog_cname_record" {
-  zone_id         = "be6fbe11f57bd4fabbf5748235a6b1b8"
-  name            = "@"
-  content         = cloudflare_pages_project.blog_pages_project.subdomain
-  type            = "CNAME"
-  proxied         = true
-  ttl             = 1
-  allow_overwrite = true
+resource "cloudflare_dns_record" "cloudflare_blog_cname_record" {
+  zone_id = "be6fbe11f57bd4fabbf5748235a6b1b8"
+  name    = "@"
+  content = cloudflare_pages_project.blog_pages_project.subdomain
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1
 }
--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Outputs
-
-output "cloudflare_blog_url" {
-  value = cloudflare_record.cloudflare_blog_cname_record.hostname
-}
